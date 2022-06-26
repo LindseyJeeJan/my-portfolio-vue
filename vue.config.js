8b807b70a0eb2f17852c693751363b997f0bfc35
Lindsey JJ <ljjlauria@mac.com>
2022-06-26 11:01:34 -0700

    vue config file update for gh pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-portfolio-vue/'
-    : '/'
-});
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/my-portfolio-vue/" : "/",
+};
